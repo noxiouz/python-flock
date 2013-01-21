eacc1b5a8e81009ec0e053b9b8693de8b727cb25
noxiouz <noxiouz@yandex.ru>
2013-01-21 14:16:13 +0400

    Debianization

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
-#!
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+        name = "zk-flock",
+        version = "0.1.0",
+        author = "Anton Tyurin",
+        author_email = "noxiouz@yandex.ru",
+        description = "Some descrip",
+        url = "https://github.com/noxiouz/python-flock",
+        license = "GPL3",
+        packages = [
+            "distributedflock",
+            "distributedflock.ZKeeperAPI"
+        ],
+        data_files = [
+            ('/usr/bin/',['zk-flock']),
+        ],
+        requires = [
+            "zookeeper"
+        ]
+)
