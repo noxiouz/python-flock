865943ed6141b9e8d46cb1b6e7bdebffa86dab2e
noxiouz <noxiouz@yandex.ru>
2013-06-23 23:38:36 +0400

    Fix some lintian's warnings

--- setup.py
+++ setup.py
@@ -8,15 +8,14 @@ setup(
         version = "0.1.0",
         author = "Anton Tyurin",
         author_email = "noxiouz@yandex.ru",
-        description = "Some descrip",
         url = "https://github.com/noxiouz/python-flock",
         license = "GPL3",
         packages = [
             "distributedflock",
             "distributedflock.ZKeeperAPI"
         ],
-        data_files = [
-            ('/usr/bin/',['zk-flock']),
+        scripts = [
+            'zk-flock'
         ],
         requires = [
             "zookeeper"
